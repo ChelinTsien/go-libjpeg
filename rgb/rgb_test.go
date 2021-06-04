797b06cea1828767769c2cba3b10266bfabf64f0
xiaoliang <jun54555@gmail.com>
2021-06-04 14:38:03 +0800

    fix

--- rgb/rgb_test.go
+++ rgb/rgb_test.go
@@ -5,7 +5,7 @@ import (
 	"image/color"
 	"testing"
 
-	"github.com/pixiv/go-libjpeg/rgb"
+	"github.com/ChelinTsien/go-libjpeg/rgb"
 )
 
 func TestImageInterface(t *testing.T) {
--- test/util/util_test.go
+++ test/util/util_test.go
@@ -3,7 +3,7 @@ package util_test
 import (
 	"testing"
 
-	"github.com/pixiv/go-libjpeg/test/util"
+	"github.com/ChelinTsien/go-libjpeg/test/util"
 )
 
 func TestOpenFile(t *testing.T) {
